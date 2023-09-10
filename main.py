dc8fa23424e86e89f8371ffd86f492b5744ef25d
Lord Basil <126736516+Drlordbasil@users.noreply.github.com>
2023-09-10 13:41:42 -0400

    Auto-formatting code

--- main.py
+++ main.py
@@ -13,11 +13,14 @@ class RecipeScraper:
 
         if response.status_code == 200:
             soup = BeautifulSoup(response.content, 'html.parser')
-            recipe_title = soup.find('h1', class_='recipe-title').get_text().strip()
+            recipe_title = soup.find(
+                'h1', class_='recipe-title').get_text().strip()
             ingredients = soup.find_all('li', class_='ingredient')
-            instructions = soup.find('div', class_='instructions').get_text().strip()
+            instructions = soup.find(
+                'div', class_='instructions').get_text().strip()
 
-            ingredient_list = [ingredient.get_text().strip() for ingredient in ingredients]
+            ingredient_list = [ingredient.get_text().strip()
+                               for ingredient in ingredients]
 
             return {
                 'title': recipe_title,
@@ -31,10 +34,12 @@ class RecipeScraper:
 class RecipeAnalyzer:
     def __init__(self, recipe_data):
         self.recipe_data = recipe_data
-        self.vectorizer = CountVectorizer().fit_transform([self.recipe_data['instructions']])
+        self.vectorizer = CountVectorizer().fit_transform(
+            [self.recipe_data['instructions']])
 
     def get_cosine_similarity(self, recipe_instructions):
-        instructions_list = [self.recipe_data['instructions'], recipe_instructions]
+        instructions_list = [
+            self.recipe_data['instructions'], recipe_instructions]
         instructions_vectorized = self.vectorizer.transform(instructions_list)
 
         cosine_similarities = cosine_similarity(instructions_vectorized)
@@ -51,7 +56,8 @@ class SustainableRecipeRecommender:
 
         for recipe in self.recipes:
             analyzer = RecipeAnalyzer(recipe['data'])
-            similarity = analyzer.get_cosine_similarity(user_preferences['instructions'])
+            similarity = analyzer.get_cosine_similarity(
+                user_preferences['instructions'])
 
             if similarity > max_similarity:
                 max_similarity = similarity
@@ -84,7 +90,8 @@ class SustainableFoodPlatform:
         })
 
     def get_top_recipes(self):
-        sorted_recipes = sorted(self.recipes, key=lambda recipe: recipe['likes'], reverse=True)
+        sorted_recipes = sorted(
+            self.recipes, key=lambda recipe: recipe['likes'], reverse=True)
         return sorted_recipes[:10]
 
     def add_comment(self, recipe_title, comment):
@@ -114,8 +121,10 @@ user_prefs = {
 }
 user_profiler.set_user_preferences(user_prefs)
 
-recipe_recommender = SustainableRecipeRecommender(sustainable_food_platform.recipes)
-recommended_recipe = recipe_recommender.recommend_recipe(user_profiler.get_user_preferences())
+recipe_recommender = SustainableRecipeRecommender(
+    sustainable_food_platform.recipes)
+recommended_recipe = recipe_recommender.recommend_recipe(
+    user_profiler.get_user_preferences())
 
 if recommended_recipe:
     print('Recommended Recipe:', recommended_recipe['data']['title'])
